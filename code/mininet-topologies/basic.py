6b61b7c82b37aab8b6a68fef583ef40a0a2c81d8
Raniita <efdezsanchez@gmail.com>
2022-01-31 23:33:05 +0100

    feat: anexo mininet

---
+++ code/mininet-topologies/basic.py
@@ -0,0 +1,15 @@
+from mininet.net import Mininet
+from mininet.topo import Topo
+
+topo = Topo()				# Empty topology
+topo.addSwitch("s1")		# Adding switch
+topo.addHost("h1")			# Add Host
+topo.addHost("h2")			# Add Host
+topo.addLink("h1", "s1")	# Link host to switch
+topo.addLink("h2", "s2")
+
+net = Mininet(topo)			# Start Mininet
+net.start()
+net.pingAll()
+net.iperf()
+net.stop()
