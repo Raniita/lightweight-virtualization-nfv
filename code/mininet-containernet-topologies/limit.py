e2d857939fb99c3c482705f5ecc6128e107e0bcb
Raniita <efdezsanchez@gmail.com>
2022-02-03 10:58:50 +0100

    feat(code): cambios a limit mininet

--- code/mininet-containernet-topologies/limit.py
+++ code/mininet-containernet-topologies/limit.py
@@ -7,17 +7,20 @@ from mininet.link import TCLink
 
 def limit_perfTest():
     net = Mininet()
-    h1 = net.addHost('h1')
-    
-    # Limit CPU at 20%
-    h2 = net.addHost('h2', cls=CPULimitedHost, cpu=0.2)
-    s1 = net.addSwitch('s1')
     c0 = net.addController('c0')
+    
+    h2 = net.addHost('h2')
+    h1 = net.addHost('h1', cls=CPULimitedHost, cpu=0.2)		# Limit CPU at 20%
+    
+    s1 = net.addSwitch('s1')
     
     # Limit BW 10mbps & delay 50ms
     net.addLink(h1,s1, cls=TCLink, bw=10, delay='50ms')
     net.addLink(h2,s1)
+    
     net.start()
+    net.pingAll()
+	net.iperf()
     CLI(net)
     net.stop()
     
